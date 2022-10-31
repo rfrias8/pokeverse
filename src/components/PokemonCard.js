8d41ab1633fd4d6bab5f1a2e51fba506c4195951
rfrias8 <rfrias8@gmail.com>
2022-10-31 16:56:47 -0500

    maps through abilities

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 
 function PokemonCard({ url, name }) {
+  const [urlInfo,setUrlInfo] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      setUrlInfo(data);
+    });
+  }, []);
+
+  console.log(urlInfo);
 
   return (
+   
     <div>
-        pokemon card
+      {urlInfo.name}
     </div>
   );
 }
