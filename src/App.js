991064621b85c67be031e9a47549c567c57f2e0d
rfrias8 <rfrias8@gmail.com>
2022-10-31 16:56:36 -0500

    maps through pokemonlist

--- src/App.js
+++ src/App.js
@@ -2,30 +2,30 @@ import React, { useEffect, useState } from 'react';
 import { Navigation } from './components/Navigation';
 import { PokemonCard } from './components/PokemonCard';
 
-const LIMIT = 150;
+const LIMIT = 10;
 const pokeApi = `https://pokeapi.co/api/v2/pokemon/?limit=${LIMIT}`;
 
 function App() {
   const [pokemonList, setPokemonList] = useState([]);
 
   useEffect( () => {
-    const fetchPokemon = async () => {
-      const res = await fetch(pokeApi);
-      const data = await res.json();
-      setPokemonList(data);
-      console.log(pokemonList)
-    }
-
-    fetchPokemon()
+    fetch(pokeApi)
+    .then((res) => res.json())
+    .then((data) => {
+      setPokemonList(data.results);
+    });
   }, []); 
+
+  console.log(pokemonList)
 
 
   return (
     <div data-testid="app">
       <Navigation />
-
       <h1>Pokemon should appear here</h1>
-      <PokemonCard />
+      {pokemonList.map((pokemon, index) => <PokemonCard  key={index} name={pokemon.name} url={pokemon.url}/> 
+      )};
+      
     </div>
   );
 }
