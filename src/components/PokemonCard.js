3cce680c767688a336f682d06ca57a9714ad35d9
rfrias8 <rfrias8@gmail.com>
2022-11-01 10:18:32 -0500

    made url and abilities visible

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,24 +1,36 @@
 import React, { useEffect, useState } from 'react';
-
+import Card from 'react-bootstrap/Card'
 
 function PokemonCard({ url, name }) {
-  const [urlInfo,setUrlInfo] = useState([]);
+  const [images,setImages] = useState('');
+  const [abilities, setAbilities] = useState([]);
 
   useEffect(() => {
     fetch(url)
     .then((res) => res.json())
     .then((data) => {
-      setUrlInfo(data);
+      setImages(data.sprites.front_default);
+      setAbilities(data.abilities);
     });
   }, []);
 
-  console.log(urlInfo);
+  console.log(images);
+  console.log(abilities);
+
 
   return (
-   
-    <div>
-      {urlInfo.name}
-    </div>
+    
+      <Card>
+        <Card.Img src={images}></Card.Img>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text as="div">
+          <ul>
+            {abilities.map(pokemonAbility => 
+              <li> {pokemonAbility.ability.name}</li>
+            )}
+          </ul>
+        </Card.Text>
+      </Card>
   );
 }
 
